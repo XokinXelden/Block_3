88cdd58beb8c6478f06c11803c20b23609f094e9
Xokin <xokinxelden@gmail.com>
2025-03-28 19:03:35 +0300

    новая часть сайта из 3 блока

---
+++ swiper.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper(".swiper", {
+  spaceBetween: 20,
+  slidesPerView: 1.5,
+  freeMode: true,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
+    clickable: true,
+  },
+});
